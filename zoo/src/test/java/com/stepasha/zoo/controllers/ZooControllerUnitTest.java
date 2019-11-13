f4f1b2eff5beaac23e2544d30e86d8477b793ac8
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-13 13:35:23 -0700

    Create ZooControllerUnitTest.java

---
+++ zoo/src/test/java/com/stepasha/zoo/controllers/ZooControllerUnitTest.java
@@ -0,0 +1,40 @@
+package com.stepasha.zoo.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZooControllerUnitTest {
+
+    @Test
+    public void listAllZoos() {
+    }
+
+    @Test
+    public void findZooById() {
+    }
+
+    @Test
+    public void findZooByName() {
+    }
+
+    @Test
+    public void updateZoo() {
+    }
+
+    @Test
+    public void addNewZoo() {
+    }
+
+    @Test
+    public void deleteZooById() {
+    }
+
+    @Test
+    public void deleteZooAnimalByIds() {
+    }
+
+    @Test
+    public void postZooAnimalByIds() {
+    }
+}
