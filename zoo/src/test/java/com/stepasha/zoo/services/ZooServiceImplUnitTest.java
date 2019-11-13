e6086fd144aef37309d2a30750b4d31b9339cba3
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-13 14:09:03 -0700

    Create ZooServiceImplUnitTest.java
    find all  test passed

---
+++ zoo/src/test/java/com/stepasha/zoo/services/ZooServiceImplUnitTest.java
@@ -0,0 +1,73 @@
+package com.stepasha.zoo.services;
+
+import com.stepasha.zoo.ZooApplication;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ZooApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ZooServiceImplUnitTest {
+
+    @Autowired
+    private ZooService zooService;
+
+    @Before
+    public void AsetUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+    @After
+    public void BtearDown() throws Exception {
+    }
+
+
+    @Test
+    public void findAllZoos() {
+        assertEquals(5, zooService.findAllZoos().size());
+    }
+
+    @Test
+    public void findZooById() {
+    }
+
+    @Test
+    public void findZooByName() {
+    }
+
+    @Test
+    public void deleteZoo() {
+    }
+
+    @Test
+    public void saveZoo() {
+    }
+
+    @Test
+    public void updateZoo() {
+    }
+
+    @Test
+    public void deleteZooAnimal() {
+    }
+
+    @Test
+    public void addZooAnimal() {
+    }
+}
